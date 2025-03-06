470157b3be2a05a725df4f44995f6365ce2d598f
garg-nikhil <nikhilgarg855@gmail.com>
2025-03-06 21:38:47 +0530

    adding selenium grid changes

--- src/main/java/Base/BaseClass.java
+++ src/main/java/Base/BaseClass.java
@@ -4,11 +4,15 @@ import Pages.com.java.nikhil.flightReservation.CustomerRegistrationPages;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -16,6 +20,8 @@ import org.testng.annotations.BeforeMethod;
 import utilities.ConfigReader;
 
 import java.io.FileInputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 public class BaseClass {
@@ -44,24 +50,53 @@ public class BaseClass {
     @BeforeMethod
     public void setupBrowser(){
         loadConfig();
-        WebDriverManager.chromedriver().setup();
+        log.info("**********************************************************************");
+        log.info("********************* Test Execution is started **********************");
+        log.info("**********************************************************************");
 
-        log.info("***********************************");
-        log.info("**** Test Execution is started ****");
-        log.info("***********************************");
+        if(Boolean.getBoolean("selenium.grid.enabled")){
+            this.driver = getRemoteDriver();
+        }else{
+            this.driver = getLocalDriver();
+        }
+
+        driver.manage().window().maximize();
+    }
+
+    private WebDriver getRemoteDriver(){
+        Capabilities capabilities;
+        if(System.getProperty("browser").equalsIgnoreCase("chrome")){
+             capabilities = new ChromeOptions();
+             log.info("Launching remote chrome browser");
+        }
+        else{
+             capabilities = new FirefoxOptions();
+            log.info("Launching remote firefox browser");
+        }
+        try {
+            return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private WebDriver getLocalDriver(){
 
         if (ConfigReader.getProperty("browser").equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
         } else if (ConfigReader.getProperty("browser").equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
         } else if (ConfigReader.getProperty("browser").equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
             driver = new EdgeDriver();
         } else {
             throw new IllegalArgumentException("Browser not supported: " + ConfigReader.getProperty("browser"));
         }
 
-        log.info("Launching "+ConfigReader.getProperty("browser"));
-        driver.manage().window().maximize();
+        log.info("Launching local "+ConfigReader.getProperty("browser"));
+        return driver;
     }
 
     @AfterMethod
